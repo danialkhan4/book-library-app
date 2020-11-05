2bf1854b5c4191d105cc332386996a5de4de3058
danialkhan4 <48042585+danialkhan4@users.noreply.github.com>
2020-11-04 22:36:24 -0800

    updated production build

---
+++ client/build/precache-manifest.69351f97792bd03ab567c0c406b6d0ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6094e1b4662e8ebc72bb9b2a2d01df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7700d1726d9cfe02e3b2",
+    "url": "/static/css/2.99a5358a.chunk.css"
+  },
+  {
+    "revision": "d501297bb15ce822ab9b",
+    "url": "/static/css/main.5b36a347.chunk.css"
+  },
+  {
+    "revision": "7700d1726d9cfe02e3b2",
+    "url": "/static/js/2.5fdf4724.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/static/js/2.5fdf4724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d501297bb15ce822ab9b",
+    "url": "/static/js/main.8a07fb0f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
